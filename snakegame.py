3b50c65b2244dd0cf93b5be9ce25cf59360011ee
robert21-meet <robert21@meet.mit.edu>
2019-07-30 12:36:37 +0300

    Stopping here

--- snakegame.py
+++ snakegame.py
@@ -7,17 +7,21 @@ startlength=6
 timestep=100
 pos_list=[]
 stamp_list=[]
-food_pos = [(100,100), (-100,100), (-100,-100), (100,-100)]
+food_pos = []
 food_stamps=[]
 snake=turtle.clone()
 snake.shape("square")
 turtle.hideturtle()
+snake.color("blue")
 
 snake.direction = "Up"
 UP_EDGE = 250
 DOWN_EDGE = -250
 RIGHT_EDGE = 400
 LEFT_EDGE = -400
+SIZE_X=400
+SIZE_Y=250
+
 #border
 border=turtle.clone()
 turtle.penup()
@@ -31,18 +35,6 @@ turtle.goto(LEFT_EDGE,UP_EDGE)
 turtle.register_shape("snakefood-1.gif")
 food = turtle.clone()
 food.shape("snakefood-1.gif")
-#foodpos
-for i in range(4):
-    food.penup()
-    food.goto(food_pos[i])
-    x=food.stamp()
-    food_stamps.append(x)
-    food.pendown()
-turtle.hideturtle()
-
-
-
-
 
 
 
@@ -68,7 +60,9 @@ def remove_stamp():
     old_stamp = stamp_list.pop(0) # last piece of tail
     snake.clearstamp(old_stamp) # erase last piece of tail
     pos_list.pop(0) # remove last piece of tail's position
-
+def noth():
+    turtle.forward(0)
+    
 
 def up():
     snake.direction="Up"
@@ -81,6 +75,8 @@ def down():
 turtle.onkeypress(down,"s")
 
 def left():
+    if snake.direction=="Right":
+        pass
     snake.direction="Left"
     print("you pressed the left key!")
 turtle.onkeypress(left,"a")
@@ -90,7 +86,26 @@ def right():
     print("you pressed the right key!")
 turtle.onkeypress(right,"d")
 
+
 turtle.listen()
+
+def makefood():
+    min_x=-int(SIZE_X/2/squaresize)+1
+    max_x=int(SIZE_X/2/squaresize)-1
+    min_y=-int(SIZE_Y/2/squaresize)+1
+    max_y=int(SIZE_Y/2/squaresize)-1
+    food.penup()
+    food_x = random.randint(min_x,max_x)*squaresize
+    food_y = random.randint(min_y,max_y)*squaresize
+    food.goto(food_x, food_y)
+    food_stamps.append(food.stamp())
+    food_pos.append(food.pos())
+    
+    
+
+
+
+
 
 
 def move_snake():
@@ -98,13 +113,16 @@ def move_snake():
     y_pos=snake.pos()[1]
     tx=snake.xcor()
     ty=snake.ycor()
-
-    if snake.pos in food_pos:
-        food_index=food_pos.index(snake.pos())
-        food.clearstamp(food_stamps[food_index])
-        food_pos.pop(food_index)
-        food_stamps.pop(food_index)
-        print("You have eaten the food!")
+    def eatfood():
+        if snake.pos() in food_pos:
+            food_index=food_pos.index(snake.pos())
+            food.clearstamp(food_stamps[food_index])
+            food_pos.pop(food_index)
+            food_stamps.pop(food_index)
+            print("You have eaten the food!")
+            makefood()
+            new_stamp()
+    eatfood()
     
     if snake.direction=="Up":
         snake.goto(x_pos,y_pos+20)
@@ -132,10 +150,10 @@ def move_snake():
         quit()
     new_stamp()
     remove_stamp()
-
     turtle.ontimer(move_snake,timestep)
-        
 
+        
+makefood()
 move_snake()
 
 
